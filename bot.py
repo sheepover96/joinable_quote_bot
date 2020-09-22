da6315640899b6043e0005e0ecd8955e0da80bde
sheepover96 <sheep.over@gmail.com>
2020-09-23 01:57:39 +0900

    Revert "tmp"
    This reverts commit 4f90fca5a1390758fe61a184d401dce95e283b5d.

--- bot.py
+++ bot.py
@@ -27,16 +27,12 @@ class Bot():
 
 def tweet(CK, CS, AT, ATS):
     quote = None
-<<<<<<< HEAD
     quote_id_list = db.session.query(Quote.id).all()
-=======
-    nquotes = db.session.query(Quote).count()
->>>>>>> parent of e8cbbc8... id bug fix
     if nquotes != 0:
         quote = None
         while quote is None:
-            rand_id = random.randint(1, nquotes) + 1
-            quote = db.session.query(Quote).get(rand_id)
+            rand_id = quote_id_list[random.randint(0, len(quote_id_list))]
+            quote = db.session.query(Quote).get(rand_id.id)
 
         url = "https://api.twitter.com/1.1/statuses/update.json"
         api = OAuth1Session(CK,CS,AT,ATS)
