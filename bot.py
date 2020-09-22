0b3e1d51655631a288a1544691f76795fc0c3c81
sheepover96 <sheep.over@gmail.com>
2020-09-23 01:51:53 +0900

    tmp

--- bot.py
+++ bot.py
@@ -27,13 +27,11 @@ class Bot():
 
 def tweet(CK, CS, AT, ATS):
     quote = None
-    nquotes = db.session.query(Quote).count()
+    quote_id_list = db.session.query(Quote.id).all()
     if nquotes != 0:
         quote = None
         while quote is None:
             rand_id = quote_id_list[random.randint(0, len(quote_id_list))]
-            print("len", len(quote_id_list))
-            print("rand_id", rand_id.id)
             quote = db.session.query(Quote).get(rand_id.id)
 
         url = "https://api.twitter.com/1.1/statuses/update.json"
